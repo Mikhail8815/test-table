9124ca008245572cc26257c9e855588ae8c38254
Mikhail8815 <mishutka150588@mail.ru>
2026-03-19 19:59:32 +0300

    feat: add add button and modal skeleton

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import type {TableItem} from "../types.ts";
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import {Button, Table} from "antd";
+import { EditOutlined, DeleteOutlined, PlusOutlined  } from '@ant-design/icons';
+import {Button, Table, Modal} from "antd";
 
 const TableComponent: React.FC = () => {
 
@@ -10,6 +10,9 @@ const TableComponent: React.FC = () => {
         { id: '2', name: 'Иван Смирнов', date: '2024-03-16', value: 3 },
         { id: '3', name: 'Серега Сергеев', date: '2024-08-12', value: 75 },
     ]);
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
 
     const columns = [
         {
@@ -48,16 +51,45 @@ const TableComponent: React.FC = () => {
         },
     ];
 
+    const handleAddClick = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
 
     return (
         <div >
             <h2>Таблица</h2>
+            <Button
+                type="primary"
+                icon={<PlusOutlined />}
+                onClick={handleAddClick}
+            >
+                Добавить
+            </Button>
             <Table
                 dataSource={data}
                 columns={columns}
                 rowKey="id"
                 pagination={false}
             />
+            <Modal
+                title="Добавить запись"
+                open={isModalOpen}
+                onCancel={handleCancel}
+                footer={[
+                    <Button key="cancel" onClick={handleCancel}>
+                        Отмена
+                    </Button>,
+                    <Button key="submit" type="primary">
+                        Сохранить
+                    </Button>,
+                ]}
+            >
+
+            </Modal>
         </div>
     );
 };
